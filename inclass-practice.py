898f5c2e1916f7f1322f9313fbf962acc6fa284f
David Soto <SotoRunner89@Davids-MacBook-Pro.local>
2023-06-21 19:11:52 -0400

    hw done

---
+++ inclass-practice.py
@@ -0,0 +1,14 @@
+
+color = input('What color is your vehicle? ')
+
+year = input('What year is your vehicle? ')
+
+make = input('Vehicle make ')
+
+model = input('Vehicle model ')
+
+
+print('{} {} {} {}'.format(color,year,make,model)) 
+
+
+
